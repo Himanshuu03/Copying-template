654c5702187103af43ad8133c7414880549dfb06
Himanshuu03 <himanshujangra2403@gmail.com>
2023-08-26 20:56:25 +0530

    js

---
+++ script.js
@@ -0,0 +1,5 @@
+const main_header_part_1 = document.querySelector(".main-header-part-1");
+
+document.querySelector(".hidden").addEventListener("click",()=>{
+        main_header_part_1.classList.toggle("active");
+})
