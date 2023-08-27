65191da67e7161a43e566fd2ef3cb1588b6646a4
Himanshuu03 <himanshujangra2403@gmail.com>
2023-08-27 22:25:25 +0530

    add

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ const main_header_part_1 = document.querySelector(".main-header-part-1");
 document.querySelector(".hidden").addEventListener("click",()=>{
         main_header_part_1.classList.toggle("active");
 })
+
+
+const quickLinks =document.querySelector(".footer-side-two");
+
+document.querySelector(".linker").addEventListener("click",()=>{
+        console.log("Hello");
+        quickLinks.classList.toggle("active");
+})
